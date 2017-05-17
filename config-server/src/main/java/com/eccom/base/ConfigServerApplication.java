0996f5f093f9aec24009f65de62af499b7e8330a
zouyanan <zouyanan@hotmail.com>
2017-05-17 16:25:41 +0800

    init config server sub module

---
+++ config-server/src/main/java/com/eccom/base/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.eccom.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by zyn on 5/17/17.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
