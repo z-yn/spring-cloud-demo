e04e00f7e3d9dcdd4f56bafd1b27c7c27e1ce210
zouyanan <zouyanan@hotmail.com>
2017-05-22 14:47:25 +0800

    add zuul and hystrix demo code

---
+++ microservice-movie-ribbon-with-hystrix/src/main/java/com/eccom/demo/HystrixApplication.java
@@ -0,0 +1,28 @@
+package com.eccom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by zyn on 5/22/17.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class HystrixApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+
+}
---
+++ mircoservice-gateway-zuul/src/main/java/com/eccom/demo/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.eccom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by zyn on 5/22/17.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
